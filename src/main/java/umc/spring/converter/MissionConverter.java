02c878972e3b6a33b3b27f3ed25b39d578e14faf
mk-star <ssunn0812@naver.com>
2024-11-25 12:40:34 +0900

    ✨ [FEAT] 특정 가게의 미션 목록 API 구현

--- src/main/java/umc/spring/converter/MissionConverter.java
+++ src/main/java/umc/spring/converter/MissionConverter.java
@@ -1,10 +1,14 @@
 package umc.spring.converter;
 
+import org.springframework.data.domain.Page;
 import umc.spring.domain.Mission;
+import umc.spring.domain.enums.MissionStatus;
 import umc.spring.web.dto.Mission.MissionRequestDTO;
 import umc.spring.web.dto.Mission.MissionResponseDTO;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class MissionConverter {
@@ -24,4 +28,26 @@ public class MissionConverter {
                 .build();
     }
 
+    public static MissionResponseDTO.MissionPreViewDTO missionPreViewDTO(Mission mission){
+        return MissionResponseDTO.MissionPreViewDTO.builder()
+                .reward(mission.getReward())
+                .status(MissionStatus.IN_PROGRESS)
+                .storeName(mission.getStore().getName())
+                .price(mission.getPrice())
+                .build();
+    }
+
+    public static MissionResponseDTO.MissionPreViewListDTO missionPreViewListDTO(Page<Mission> missionList) {
+        List<MissionResponseDTO.MissionPreViewDTO> missionPreViewDTOList = missionList.stream()
+                .map(MissionConverter::missionPreViewDTO).collect(Collectors.toList());
+
+        return MissionResponseDTO.MissionPreViewListDTO.builder()
+                .isLast(missionList.isLast())
+                .isFirst(missionList.isFirst())
+                .totalPage(missionList.getTotalPages())
+                .totalElements(missionList.getTotalElements())
+                .listSize(missionPreViewDTOList.size())
+                .missionList(missionPreViewDTOList)
+                .build();
+    }
 }
--- src/main/java/umc/spring/service/StoreService/StoreQueryServiceImpl.java
+++ src/main/java/umc/spring/service/StoreService/StoreQueryServiceImpl.java
@@ -5,8 +5,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import umc.spring.domain.Mission;
 import umc.spring.domain.Review;
 import umc.spring.domain.Store;
+import umc.spring.repository.MissionRepository.MissionRepository;
 import umc.spring.repository.ReviewRepository.ReviewRepository;
 import umc.spring.repository.StoreRepository.StoreRepository;
 
@@ -19,6 +21,7 @@ import java.util.Optional;
 public class StoreQueryServiceImpl implements StoreQueryService{
     private final StoreRepository storeRepository;
     private final ReviewRepository reviewRepository;
+    private final MissionRepository missionRepository;
 
     @Override
     public Optional<Store> findStore(Long id) {
@@ -28,9 +31,7 @@ public class StoreQueryServiceImpl implements StoreQueryService{
     @Override
     public List<Store> findStoresByNameAndScore(String name, Float score) {
         List<Store> filteredStores = storeRepository.dynamicQueryWithBooleanBuilder(name, score);
-
         filteredStores.forEach(store -> System.out.println("Store: " + store));
-
         return filteredStores;
     }
 
@@ -44,5 +45,12 @@ public class StoreQueryServiceImpl implements StoreQueryService{
         Store store = storeRepository.findById(storeId).get();
         Page<Review> storePage = reviewRepository.findAllByStore(store, PageRequest.of(page, 10));
         return storePage;
+    }
+
+    @Override
+    public Page<Mission> getMissionList(Long storeId, Integer page) {
+        Store store = storeRepository.findById(storeId).get();
+        Page<Mission> missionPage = missionRepository.findAllByStore(store, PageRequest.of(page - 1, 10));
+        return missionPage;
     }
 }
